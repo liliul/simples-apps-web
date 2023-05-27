c3840e56a4d2eedabfbf78682370225b941c4f24
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-05-27 19:38:05 -0300

    add: arquivo api.js

---
+++ climaTempo/src/hook/api.js
@@ -0,0 +1,4 @@
+//  https://openweathermap.org/current
+//  https://openweathermap.org/current#multi
+//  https://home.openweathermap.org/api_keys
+//  romero ark sirome4179@farebus.com senha:romero1234?
