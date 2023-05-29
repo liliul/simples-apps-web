d1bc0b597d78ac565963f5bf27c5b98efc7c181e
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-05-29 16:34:20 -0300

    add: utils.js

---
+++ climaTempo/src/hook/utils.js
@@ -0,0 +1,7 @@
+export function kelvinCelsius(tempDay) {
+    // const resTemp = tempDay - 273.15
+    // const resMax  = tempMax - 273.15
+    // const resMin  = tempMin - 273.15
+    // return resTemp, resMax, resMin;
+    return tempDay - 273.15
+}
