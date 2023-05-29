b6719d829a1ec46ccda91848920ec14362595812
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-05-28 23:10:55 -0300

    feat:  getApi

--- climaTempo/src/hook/api.js
+++ climaTempo/src/hook/api.js
@@ -2,3 +2,10 @@
 //  https://openweathermap.org/current#multi
 //  https://home.openweathermap.org/api_keys
 //  romero ark sirome4179@farebus.com senha:romero1234?
+
+async function getApi() {
+  const req = await fetch(url)
+  const res = await req.json()
+  
+  console.log(res)
+}
