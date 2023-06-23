d9dd2edbe67d3f94243e3a1bf4df5e51ed8a6027
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-06-23 17:47:18 -0300

    feat: getApis() teste funcionando

--- climaTempo/src/hook/geolocation.js
+++ climaTempo/src/hook/geolocation.js
@@ -6,19 +6,21 @@ import {
 
 const geo = () => {
     if(navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(pos) {
+      navigator.geolocation.getCurrentPosition(async function(pos) {
         console.log(pos.coords.latitude, pos.coords.longitude)
 
-         
+         await getApis(pos.coords.latitude, pos.coords.longitude)
       })
     }
   }
-  //geo()
+  geo()
 
   async function getApis(lat, lon) {
     const [reqApi1, reqApi2, reqApi3] = await Promise.all([
       fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${TOKEN_API_OPEN_WEATHER}&units=metric&lang=pt_br`),
       fetch(`${URL_AR}lat=${lat}&lon=${lon}${APPID_TOKEN}${TOKEN_API_OPEN_WEATHER}&lang=pt_br`),
-      fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APPID_TOKEN}&units=metric&lang=pt_br`),
+      fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${TOKEN_API_OPEN_WEATHER}&units=metric&lang=pt_br`),
     ])
+
+    console.log(reqApi1, reqApi2, reqApi3);
   }
