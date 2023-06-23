29437d843f2b0364abcf3ec05c23713330ba64e0
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-06-23 17:03:36 -0300

    feat: função getApis

--- climaTempo/src/hook/geolocation.js
+++ climaTempo/src/hook/geolocation.js
@@ -2,7 +2,16 @@ const geo = () => {
     if(navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(function(pos) {
         console.log(pos.coords.latitude, pos.coords.longitude)
+
+         
       })
     }
   }
   //geo()
+
+  async function getApis() {
+    const [reqApi1, reqApi2] = await Promise.all([
+      fetch(`https://api.openweathermap.org/data/2.5/weather?lat=57&lon=-2.15&appid=9033de8545dfa66184beaa29576f414b&units=metric&lang=pt_br`),
+      fetch(),
+    ])
+  }
