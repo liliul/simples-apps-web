33435e990bae117ed0eb930a7d9457fa6adc9a6f
liliul <liliuestudo@gmail.com>
2023-06-28 21:39:49 -0300

    fix: conflito na url reqApi1 parametro units=metric com função kelvinCelsius()

--- climaTempo/src/hook/geolocation.js
+++ climaTempo/src/hook/geolocation.js
@@ -2,34 +2,34 @@ import { TOKEN_API_OPEN_WEATHER, APPID_TOKEN, URL_AR } from './token.js';
 
 import { tempNow, airQuality, visible ,sunTime, weather, openWeatherMap } from './htmlRender.js';
 
-// geo()
 
 const geo = () => {
-    if(navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(async function(pos) {
-        console.log(pos.coords.latitude, pos.coords.longitude)
-
-         await getApis(pos.coords.latitude, pos.coords.longitude)
-      })
-    }
-}
-
-  async function getApis(lat, lon) {
-    const [reqApi1, reqApi2, reqApi3] = await Promise.all([
-      fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${TOKEN_API_OPEN_WEATHER}&units=metric&lang=pt_br`),
-      fetch(`${URL_AR}lat=${lat}&lon=${lon}${APPID_TOKEN}${TOKEN_API_OPEN_WEATHER}&lang=pt_br`),
-      fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${TOKEN_API_OPEN_WEATHER}&units=metric&lang=pt_br`),
-    ])
-
-    const resApi1 = await reqApi1.json();
-    const resApi2 = await reqApi2.json();
-    const resApi3 = await reqApi3.json();
-    console.log(resApi1, resApi2, resApi3);
-
-    tempNow(resApi1)
-    airQuality(resApi2)
-    weather(reqApi3)
-    visible(resApi1)
-    sunTime(resApi1)   
-    openWeatherMap(resApi1)
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(async function(pos) {
+      console.log(pos.coords.latitude, pos.coords.longitude)
+      
+      await getApis(pos.coords.latitude, pos.coords.longitude)
+    })
   }
+}
+geo()
+
+async function getApis(lat, lon) {
+  const [reqApi1, reqApi2, reqApi3] = await Promise.all([
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${TOKEN_API_OPEN_WEATHER}&lang=pt_br`),
+    fetch(`${URL_AR}lat=${lat}&lon=${lon}${APPID_TOKEN}${TOKEN_API_OPEN_WEATHER}&lang=pt_br`),
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${TOKEN_API_OPEN_WEATHER}&units=metric&lang=pt_br`),
+  ])
+
+  const resApi1 = await reqApi1.json();
+  const resApi2 = await reqApi2.json();
+  const resApi3 = await reqApi3.json();
+  console.log(resApi1, resApi2, resApi3);
+
+  tempNow(resApi1)
+  airQuality(resApi2)
+  weather(reqApi3)
+  visible(resApi1)
+  sunTime(resApi1)   
+  openWeatherMap(resApi1)
+}
