638ae47cb091a7bb4119af185f132fc70ba6c3b6
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-09-02 19:56:20 -0300

    add: renderCard.ts no index components

--- apiRandomUser/src/components/index.ts
+++ apiRandomUser/src/components/index.ts
@@ -1,41 +1,42 @@
 import {getApiRandomUser} from '../api/index.ts';
+import RenderCard from '../pages/renderCard.ts';
 
 export async function CardIndex() {
 	const randomUser =  await getApiRandomUser();
 	console.log(randomUser, 'rr')
-	randomUser.forEach((getApi) => renderCard(getApi))
+	randomUser.forEach((getApi) => RenderCard(getApi))
 
 	return randomUser			  
 }
 
-function renderCard(api) {
-	const {cell, dob, email, gender, id, location, name, phone, picture, nat} = api;
+// function renderCard(api) {
+// 	const {cell, dob, email, gender, id, location, name, phone, picture, nat} = api;
 
-	document.querySelector<HTMLDivElement>("#card").innerHTML +=`
-		<article class='cards'data-id='${id.value}'>
-			<section class='c-img'>
-				<img src='${picture.medium}' alt='Img' />
-			</section>
+// 	document.querySelector<HTMLDivElement>("#card").innerHTML +=`
+// 		<article class='cards'data-id='${id.value}'>
+// 			<section class='c-img'>
+// 				<img src='${picture.medium}' alt='Img' />
+// 			</section>
 
-			<section class='names'>
-				${name.title} ${name.first} ${name.last}
-			</section>
+// 			<section class='names'>
+// 				${name.title} ${name.first} ${name.last}
+// 			</section>
 
-			<section class='local'>
-				<b>${location.country}</b>
-				<b>${location.state}</b>
-				<b>${location.city}</b>
-			</section>
+// 			<section class='local'>
+// 				<b>${location.country}</b>
+// 				<b>${location.state}</b>
+// 				<b>${location.city}</b>
+// 			</section>
 
-			<section class='infos'>
-				<span>${nat}</span>
-				<span>${gender}</span>
-				<span>${phone}</span>
-				<span>${email}</span>
-				<span>${cell}</span>
-				<span>${dob.age}</span>
-			</section>
-		</article>
+// 			<section class='infos'>
+// 				<span>${nat}</span>
+// 				<span>${gender}</span>
+// 				<span>${phone}</span>
+// 				<span>${email}</span>
+// 				<span>${cell}</span>
+// 				<span>${dob.age}</span>
+// 			</section>
+// 		</article>
 
-	`
-}
+// 	`
+// }
