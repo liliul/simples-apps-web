f401f17b02612a812e100a128cb8a7268877abf5
liliul <liliuestudo@gmail.com>
2023-09-04 23:37:30 -0300

    chore: removendo comentarios

--- apiRandomUser/src/components/index.ts
+++ apiRandomUser/src/components/index.ts
@@ -8,35 +8,3 @@ export async function CardIndex() {
 
 	return randomUser			  
 }
-
-// function renderCard(api) {
-// 	const {cell, dob, email, gender, id, location, name, phone, picture, nat} = api;
-
-// 	document.querySelector<HTMLDivElement>("#card").innerHTML +=`
-// 		<article class='cards'data-id='${id.value}'>
-// 			<section class='c-img'>
-// 				<img src='${picture.medium}' alt='Img' />
-// 			</section>
-
-// 			<section class='names'>
-// 				${name.title} ${name.first} ${name.last}
-// 			</section>
-
-// 			<section class='local'>
-// 				<b>${location.country}</b>
-// 				<b>${location.state}</b>
-// 				<b>${location.city}</b>
-// 			</section>
-
-// 			<section class='infos'>
-// 				<span>${nat}</span>
-// 				<span>${gender}</span>
-// 				<span>${phone}</span>
-// 				<span>${email}</span>
-// 				<span>${cell}</span>
-// 				<span>${dob.age}</span>
-// 			</section>
-// 		</article>
-
-// 	`
-// }
--- apiRandomUser/src/main.ts
+++ apiRandomUser/src/main.ts
@@ -1,5 +1,4 @@
 import './style.css'
-// import './api/index.ts'
 import {Card} from './layout/card.ts'
 
 document.querySelector<HTMLDivElement>('#app').innerHTML = `${Card()}`
