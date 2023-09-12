cfe56b3806e08185b15905c2ac23ce09797dce35
liliul <liliuestudo@gmail.com>
2023-09-12 11:27:09 -0300

    chore: testando codigo

--- jsCrud/src/main.js
+++ jsCrud/src/main.js
@@ -1,1 +1,14 @@
-console.log('js')
+import {search} from "./service/api.js"
+
+const input = document.getElementById('search')
+
+// input.addEventListener('change', async (e) => {
+//     const u = await search.searchGithub(input.value)
+//     console.log(u)
+// })
+
+async function api(sa) {
+    const name = await search.searchGithub(sa)
+    return name.id
+}
+console.log(api('diego3g'))
--- jsCrud/src/service/api.js
+++ jsCrud/src/service/api.js
@@ -1,6 +1,11 @@
-export default async function searchGithub(user) {
+async function searchGithub(user) {
     const req = await fetch(`https://api.github.com/users/${user}`)
-    const res = await req.json()
+    const {name, login, id} = await req.json()
 
-    return res;
+    // const {name, login, id} = res; 
+    return console.log(name, login, id);
+}
+
+export const search = {
+    searchGithub
 }
