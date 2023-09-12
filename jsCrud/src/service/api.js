7d12892acd2c4548b37f7cde92d18ee377dae3d5
liliul <liliuestudo@gmail.com>
2023-09-12 12:11:25 -0300

    fix: retornando response da api obrigatorio

--- jsCrud/src/service/api.js
+++ jsCrud/src/service/api.js
@@ -1,9 +1,7 @@
 async function searchGithub(user) {
     const req = await fetch(`https://api.github.com/users/${user}`)
-    const {name, login, id} = await req.json()
-
-    // const {name, login, id} = res; 
-    return console.log(name, login, id);
+    
+    return await req.json()
 }
 
 export const search = {
