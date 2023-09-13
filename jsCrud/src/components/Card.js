a167f3dd13f6bb28b9284e8a1f8beb4310b1c92c
liliul <liliuestudo@gmail.com>
2023-09-12 22:35:36 -0300

    fix: função removeButton

--- jsCrud/src/components/Card.js
+++ jsCrud/src/components/Card.js
@@ -6,7 +6,7 @@ export default function Card(id, avatar,name, login) {
             <h4 class='h4'>${login}</h4>
             
             <div class='button'>
-                <button id='${id}'>Excluir</button>
+                <button id='del' data-id='${id}'>Excluir</button>
             </div>
         </section>
     `)
--- jsCrud/src/main.js
+++ jsCrud/src/main.js
@@ -1,14 +1,23 @@
 import {search} from "./service/api.js"
+import Card from "./components/Card.js"
 
 const input = document.getElementById('search')
 
-// input.addEventListener('change', async (e) => {
-//     const u = await search.searchGithub(input.value)
-//     console.log(u)
-// })
+input.addEventListener('change', async (e) => {
+    const w = await search.searchGithub(input.value)
+    console.log(w)
+    const json = {
+        'id':w.id,
+        'avatar':w.avatar_url,
+        'name':w.name,
+        'login':w.login
 
-async function api(sa) {
-    const name = await search.searchGithub(sa)
-    return name.id
-}
-console.log(api('diego3g'))
+    }
+
+    document.getElementById('container').innerHTML +=Card(json.id,json.avatar,json.name,json.login)
+    
+})
+// function removeButton() {
+//     const del = document.getElementById('del')
+//     del.remove()
+// }
