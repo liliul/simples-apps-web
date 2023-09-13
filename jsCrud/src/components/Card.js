3c96761f3c3221c652fe7d2470cb195551c02cfb
liliul <liliuestudo@gmail.com>
2023-09-13 20:27:25 -0300

    style: card

--- jsCrud/src/components/Card.js
+++ jsCrud/src/components/Card.js
@@ -1,9 +1,11 @@
 export default function Card(id, avatar,name, login) {
     return (`
         <section class='c-card' data-id='${id}'>
-            <img src='${avatar}' />
-            <h1 class='h1'>${name}></h1>
-            <h4 class='h4'>${login}</h4>
+            <article class='name'>
+                <img src='${avatar}' />
+                <h1 class='h1'>${name}></h1>
+                <h4 class='h4'>${login}</h4>
+            </article>
             
             <div class='button'>
                 <button id='del' data-id='${id}'>Excluir</button>
