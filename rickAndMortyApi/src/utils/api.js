14d56f646168edc6e0fc875d54f5a3c6d2c671ec
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-09-30 15:44:58 -0300

    create: utils

---
+++ rickAndMortyApi/src/utils/api.js
@@ -0,0 +1,10 @@
+export const fetchApi = (value) => {
+  const result = fetch(`https://rickandmortyapi.com/api/character/${value}`)
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+    return data;
+  });
+
+  return result;
+}
