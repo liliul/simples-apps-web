7288d8b4186b00d04d6289674d5d24cc407b58f2
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-09-30 15:56:19 -0300

    create: keys.js

---
+++ rickAndMortyApi/src/utils/keys.js
@@ -0,0 +1,22 @@
+const keys = [
+  'name',
+  'status', 
+  'species', 
+  'gender', 
+  'origin', 
+  'episode'
+];
+
+const newKeys = {
+  name: 'Nome',
+  status: 'Status',
+  species: 'Espécie',
+  gender: 'Gênero',
+  origin: 'Planeta de origem',
+  episode: 'Episódios',
+}
+
+export const KEYS = {
+  keys,
+  newKeys 
+}
