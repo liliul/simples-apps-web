df46dfecb9ecfbce4e084dc1c00e1e2b9eaecb72
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-09-30 15:57:42 -0300

    add: KEYS no script.js

--- rickAndMortyApi/src/script.js
+++ rickAndMortyApi/src/script.js
@@ -1,5 +1,6 @@
 import { fetchApi } from './utils/api.js';
-
+import { KEYS } from './utils/keys.js';
+ 
 const characterId = document.getElementById('characterId');
 const btnGo = document.getElementById('btn-go');
 const btnReset = document.getElementById('btn-reset');
@@ -8,32 +9,24 @@ const conteinerResult = document.getElementById('result-style');
 const image = document.getElementById('img');
 
 
-const keys = ['name', 'status', 'species', 'gender', 'origin', 'episode'];
-const newKeys = {
-  name: 'Nome',
-  status: 'Status',
-  species: 'Espécie',
-  gender: 'Gênero',
-  origin: 'Planeta de origem',
-  episode: 'Episódios',
-}
+
 
 const buildResult = (result) => {
-  return keys.map((key) => document.getElementById(key))
+  return KEYS.keys.map((key) => document.getElementById(key))
     .map((elem) => {
       if(elem.checked === true && (Array.isArray(result[elem.name])) === true){
         const arrayResult = result[elem.name].join('\r\n');
         console.log(arrayResult);
         const newElem = document.createElement('p');
-        newElem.innerHTML = `${newKeys[elem.name]}: ${arrayResult}`;
+        newElem.innerHTML = `${KEYS.newKeys[elem.name]}: ${arrayResult}`;
         content.appendChild(newElem);
       } else if(elem.checked === true && (elem.name === 'origin')){
         const newElem = document.createElement('p');
-        newElem.innerHTML = `${newKeys[elem.name]}: ${result[elem.name].name}`;
+        newElem.innerHTML = `${KEYS.newKeys[elem.name]}: ${result[elem.name].name}`;
         content.appendChild(newElem);
       } else if(elem.checked === true && typeof(result[elem.name]) !== 'object'){
         const newElem = document.createElement('p');
-        newElem.innerHTML = `${newKeys[elem.name]}: ${result[elem.name]}`;
+        newElem.innerHTML = `${KEYS.newKeys[elem.name]}: ${result[elem.name]}`;
         content.appendChild(newElem);
       }
     });
