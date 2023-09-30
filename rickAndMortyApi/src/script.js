1f14ea21dfc1d357f79f7a8c22a209c0d40e5ecd
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-09-30 19:11:14 -0300

    chore: removendo console.log

--- rickAndMortyApi/src/script.js
+++ rickAndMortyApi/src/script.js
@@ -9,23 +9,26 @@ const conteinerResult = document.getElementById('result-style');
 const image = document.getElementById('img');
 
 
-
-
 const buildResult = (result) => {
   return KEYS.keys.map((key) => document.getElementById(key))
     .map((elem) => {
+      
       if(elem.checked === true && (Array.isArray(result[elem.name])) === true){
         const arrayResult = result[elem.name].join('\r\n');
-        console.log(arrayResult);
+       
         const newElem = document.createElement('p');
         newElem.innerHTML = `${KEYS.newKeys[elem.name]}: ${arrayResult}`;
         content.appendChild(newElem);
+      
       } else if(elem.checked === true && (elem.name === 'origin')){
         const newElem = document.createElement('p');
+      
         newElem.innerHTML = `${KEYS.newKeys[elem.name]}: ${result[elem.name].name}`;
         content.appendChild(newElem);
+      
       } else if(elem.checked === true && typeof(result[elem.name]) !== 'object'){
         const newElem = document.createElement('p');
+      
         newElem.innerHTML = `${KEYS.newKeys[elem.name]}: ${result[elem.name]}`;
         content.appendChild(newElem);
       }
@@ -40,10 +43,12 @@ btnGo.addEventListener('click', async (event) => {
   }
 
   const result = await fetchApi(characterId.value);
+  
   if(content.firstChild === null){
     conteinerResult.className = 'result-style';
     image.src = `${result.image}`;
     buildResult(result);
+  
   } else {
     content.innerHTML = '';
     conteinerResult.className = 'result-style';
--- rickAndMortyApi/src/utils/api.js
+++ rickAndMortyApi/src/utils/api.js
@@ -2,7 +2,6 @@ export const fetchApi = (value) => {
   const result = fetch(`https://rickandmortyapi.com/api/character/${value}`)
   .then((res) => res.json())
   .then((data) => {
-    console.log(data);
     return data;
   });
 
