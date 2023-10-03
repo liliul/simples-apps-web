68baf91809a26785364ae8a1520515517611470f
liliul <liliuestudo@gmail.com>
2023-10-03 19:47:19 -0300

    feat: component footer

---
+++ proGithub/src/components/footer.js
@@ -0,0 +1,37 @@
+export function Footer() {
+	const footerContainer = document.createElement('footer');
+	footerContainer.classList.add('footer');
+
+	footerContainer.innerHTML = `
+        <div class="icon-rede">
+            <h2 class="text-footer">Redes social</h2>
+            <div class="icons">
+                <a href="#">
+                    <div class="icon-img">
+                        <img src="./assets/linkedin.svg" >
+                    </div>
+                </a>
+
+                <a href="#">
+                    <div class="icon-img">
+                        <img src="./assets/whatsapp.svg" >
+                    </div>
+                </a>
+
+                <a href="#">
+                    <div class="icon-img">
+                        <img src="./assets/instagram.svg" >
+                    </div>
+                </a>
+                
+                <a href="#">
+                    <div class="icon-img">
+                        <img src="./assets/mail.svg" >
+                    </div>
+                </a>
+            </div>
+        </div>
+	`
+
+	document.querySelector('#root').appendChild(footerContainer);
+}
