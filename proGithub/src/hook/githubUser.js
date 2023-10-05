5f8ce02db859a1f9900c1c5f43de7ba0a4b73b5c
liliul <liliuestudo@gmail.com>
2023-10-05 09:02:21 -0300

    fix: passando importes de ids na função User

---
+++ proGithub/src/hook/githubUser.js
@@ -0,0 +1,43 @@
+import { URL } from '../service/url.js';
+
+const urlTwitter = `${URL.URL_TWITTER}`;
+let http = 'https://';
+
+export function User(data) {
+    const nome = document.querySelector('#userNome')
+    const avatar = document.querySelector('#avatar')
+    const bio = document.querySelector('#bio')
+
+    const linkGithub = document.querySelector('#link-github')
+    const linkBlog = document.querySelector('#link-blog')
+
+    const local = document.querySelector('#local')
+    const company = document.querySelector('#company')
+    const twitter = document.querySelector('#twitter')
+    const followers = document.querySelector('#followers')
+    const following = document.querySelector('#following')
+    const reposPublic = document.querySelector('#repo-public')
+    const gistsPublic = document.querySelector('#gists-public')
+
+	nome.innerText = data.name
+    avatar.src = data.avatar_url
+
+    linkGithub.href = data.html_url
+    bio.innerText = data.bio
+
+    const bloglink = data.blog
+    linkBlog.href = `${http}${bloglink}`
+
+    local.innerText = data.location
+    company.innerText = data.company
+    
+    const twitterlink = data.twitter_username
+    twitter.innerText = data.twitter_username
+    twitter.href = `${urlTwitter}${twitterlink}`
+    
+    followers.innerText = data.followers
+    following.innerText = data.following
+
+    reposPublic.innerText = data.public_repos
+    gistsPublic.innerText = data.public_gists
+}
