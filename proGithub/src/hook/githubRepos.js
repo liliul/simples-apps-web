8075887554eeea36c13480548d081b7e41cb66a4
liliul <liliuestudo@gmail.com>
2023-10-05 13:34:45 -0300

    feat: Repos

---
+++ proGithub/src/hook/githubRepos.js
@@ -0,0 +1,29 @@
+export function Repos(grepos) {
+	const cards = document.querySelector('#cards')
+
+	grepos.map(list => {
+        const nameRepo = document.createElement('h1')
+        const htmlUrl = document.createElement('a')
+        const article = document.createElement('article')
+        const description = document.createElement('p')
+        const language = document.createElement('span')
+
+        
+        htmlUrl.href = list.html_url
+
+        article.classList.add('card')
+
+        nameRepo.innerHTML = list.name
+        description.innerHTML = list.description
+        description.classList.add('s-desc')
+        language.innerHTML = list.language
+        language.classList.add('lang')
+
+        cards.appendChild(htmlUrl)
+        htmlUrl.appendChild(article)
+        article.appendChild(nameRepo)
+        article.appendChild(description)
+        article.appendChild(language)
+
+    })   
+}
