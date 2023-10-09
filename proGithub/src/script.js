e0db92658898458e0e1e9a257481bebfa70eaa72
liliul <liliuestudo@gmail.com>
2023-10-09 17:34:06 -0300

    chore: clear code

--- proGithub/src/script.js
+++ proGithub/src/script.js
@@ -5,6 +5,7 @@ import { Repos } from './hook/githubRepos.js';
 import { Header } from './components/header.js';
 import { Main } from './components/main.js';
 import { Footer } from './components/footer.js';
+
 Header()
 Main()
 Footer()
@@ -15,11 +16,13 @@ async function user() {
     console.log('use',data)
     User(data)
 }
+
 user()
 
 async function repos() {
-     const grepos = await FetchApi.apiGithubRepos();
-     console.log('repos', grepos)
-     Repos(grepos)
+    const grepos = await FetchApi.apiGithubRepos();
+    console.log('repos', grepos)
+    Repos(grepos)
 }
+
 repos()
