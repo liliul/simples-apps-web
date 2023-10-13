8b38db1f2525bd07bb5c5d3efda0d72e35df65b7
liliul <liliuestudo@gmail.com>
2023-10-13 20:29:32 -0300

    chore: jsdocs na config

--- proGithub/src/config/config.js
+++ proGithub/src/config/config.js
@@ -1,5 +1,13 @@
+/**
+ * @description ### Aqui fica as configurações do protifolio
+*/
+
 const githubUser = 'diego3g';
 
+/**
+ * @param {object} userConfig
+ * @description ### exportando configurações do usuario para modificar
+*/
 
 export const userConfig = {
 	githubUser
