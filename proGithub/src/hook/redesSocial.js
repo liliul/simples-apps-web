f390427ed3f8f80a78ebbad1ee8d6b91223932a4
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-10-21 17:53:20 -0300

    create: hook redesSocial

---
+++ proGithub/src/hook/redesSocial.js
@@ -0,0 +1,11 @@
+import { userConfig } from '../config/config.js';
+
+const redesSocial = () => {
+    return (`
+        <div class="icons">
+            ${userConfig.redeSocial}
+        </div>
+    `)
+}
+
+export default redesSocial;
