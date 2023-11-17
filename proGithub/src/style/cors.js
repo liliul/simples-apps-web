7f3378fa152a60fe95e765d329f19de67f479700
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-11-17 18:58:00 -0300

    chore: jsdoc no cors.js proGithub

--- proGithub/src/style/cors.js
+++ proGithub/src/style/cors.js
@@ -1,5 +1,9 @@
 import { userConfig } from '../config/config.js';
 
+/**
+ * @description ### codigo que mudo a cor vindo da config do usuario
+ */
+
 let txt = "--bg-techs"
 
 document.documentElement.style.setProperty(txt, userConfig.textoCor);
