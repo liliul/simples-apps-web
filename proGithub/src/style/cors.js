755529d1fa22d008c098fad2d868e1c259a66765
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-11-18 16:13:32 -0300

    feat: cors munda a cor da variavel --bg-lang proGithub

--- proGithub/src/style/cors.js
+++ proGithub/src/style/cors.js
@@ -4,6 +4,8 @@ import { userConfig } from '../config/config.js';
  * @description ### codigo que mudo a cor vindo da config do usuario
  */
 
-let txt = "--bg-techs"
+let txt  = "--bg-techs";
+let lang = "--bg-lang";
 
 document.documentElement.style.setProperty(txt, userConfig.textoCor);
+document.documentElement.style.setProperty(lang, userConfig.textoCor);
