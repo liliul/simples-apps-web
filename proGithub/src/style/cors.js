7e38f8948ed5d4e5eecb8cb7ab60b5ee89872ad5
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-11-18 17:38:27 -0300

    create: a variavel css --card-hover-cor proGithub

--- proGithub/src/style/cors.js
+++ proGithub/src/style/cors.js
@@ -6,6 +6,17 @@ import { userConfig } from '../config/config.js';
 
 let txt  = "--bg-techs";
 let lang = "--bg-lang";
+let cardHover = "--card-hover-cor";
 
 document.documentElement.style.setProperty(txt, userConfig.textoCor);
 document.documentElement.style.setProperty(lang, userConfig.textoCor);
+document.documentElement.style.setProperty(cardHover, userConfig.cardHoverCor);
+
+
+/*
+	Rascunho
+
+	salmon fa7f72
+	salmonDark E9967A
+
+*/
