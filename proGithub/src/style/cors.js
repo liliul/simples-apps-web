abbcd262c7fb091498a8fe73839e483763c88c7d
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-11-19 11:43:03 -0300

    feat: mudando a cor da variavel css --text-p-skills proGithub

--- proGithub/src/style/cors.js
+++ proGithub/src/style/cors.js
@@ -4,13 +4,15 @@ import { userConfig } from '../config/config.js';
  * @description ### codigo que mudo a cor vindo da config do usuario
  */
 
-let txt  = "--bg-techs";
-let lang = "--bg-lang";
-let cardHover = "--card-hover-cor";
+let txt        = "--bg-techs";
+let lang       = "--bg-lang";
+let cardHover  = "--card-hover-cor";
+let txtPskills = "--text-p-skills";
 
 document.documentElement.style.setProperty(txt, userConfig.textoCor);
 document.documentElement.style.setProperty(lang, userConfig.textoCor);
 document.documentElement.style.setProperty(cardHover, userConfig.cardHoverCor);
+document.documentElement.style.setProperty(txtPskills, userConfig.txtPskills);
 
 
 /*
@@ -18,6 +20,9 @@ document.documentElement.style.setProperty(cardHover, userConfig.cardHoverCor);
 
 	salmon fa7f72
 	salmonDark E9967A
+	salmonLight #FFA07A
+
+	quantum #111111
 
 	azul #75D7EC
 */
