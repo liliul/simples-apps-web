e08ccd4c13faab3b35607d5d78b5e44f83b0451a
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-11-30 14:04:54 -0300

    feat: getDados frontCrudApi

--- frontCrudApi/frontend/main.js
+++ frontCrudApi/frontend/main.js
@@ -1,1 +1,9 @@
 const urlApiCrud = 'http://localhost:5500/api';
+
+// Crie uma função para fazer uma requisição GET para buscar os dados do servidor:
+function getDados() {
+    fetch('url-do-servidor')
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.error(error))
+}
