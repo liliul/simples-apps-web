b33b9644e242d0552d90b9f42ea5747b3714dfc4
liliul <liliuestudo@gmail.com>
2023-12-06 22:24:09 -0300

    feat: updateInterface atualizando infos do usuario frontCrudApi

--- frontCrudApi/frontend/main.js
+++ frontCrudApi/frontend/main.js
@@ -41,6 +41,20 @@ idButtonUpdate.addEventListener('click', (e) => {
     
 })
 
+function updateInterface() {
+    const idNameUp   = document.getElementById('nameUp');
+    const idCityUp   = document.getElementById('cityUp');
+    const idAvatarUp = document.getElementById('avatarUp');
+    const idUpdate = document.getElementById('idUpdate');
+
+    const idsJson  = {
+        name: idNameUp.value,
+        avatar: idAvatarUp.value,
+        city: idCityUp.value
+    }
+    console.log(idsJson, idUpdate.valueAsNumber)
+    atualizarDados(idUpdate.valueAsNumber, idsJson)
+}
 
 // Crie uma função para fazer uma requisição GET para buscar os dados do servidor:
 function getDados() {
