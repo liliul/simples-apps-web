7fb39fa715a5cd25072fb2ef4f89e14420ff309c
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-12-12 00:43:05 -0300

    add: url da api no reqDELETE frontCrudApi

--- frontCrudApi/frontend/service/reqDELETE.js
+++ frontCrudApi/frontend/service/reqDELETE.js
@@ -1,3 +1,5 @@
+const urlApiCrud = 'http://localhost:5500/api';
+
 // Crie uma função para deletar dados no servidor usando uma requisição DELETE:
 export default window.deletarDados = function(id) {
     fetch(`${urlApiCrud}/${id}`, {
