3b44fbb2e6c0526f233fe717b0c6804e4efc2950
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-12-11 21:43:19 -0300

    create: hook enviandoDados frontCrudApi

---
+++ frontCrudApi/frontend/hook/enviandoDados.js
@@ -0,0 +1,22 @@
+import enviarDados from '../service/reqPOST.js';
+
+const idButtonForm = document.getElementById('buttonForm');
+const idName = document.getElementById('name');
+const idCity = document.getElementById('city');
+const idAvatar = document.getElementById('avatar');
+
+export default function EnviarDadosParaApi() {
+
+	idButtonForm.addEventListener('click', (e) => {
+        e.defaultPrevented
+
+        const idsJson = {
+            name: idName.value,
+            avatar: idAvatar.value,
+            city: idCity.value
+        }
+
+        console.log(idsJson)
+        enviarDados(idsJson)
+    })
+}
