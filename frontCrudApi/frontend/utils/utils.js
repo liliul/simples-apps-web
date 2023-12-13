5115b6cfcdf79a109716e342c02625c6382a88c5
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-12-13 16:22:03 -0300

    chore: dados de teste frontCrudApi

--- frontCrudApi/frontend/utils/utils.js
+++ frontCrudApi/frontend/utils/utils.js
@@ -3,3 +3,41 @@ export function ButtonTxt(){
         <b>Comfirmar Update</b>
     `)
 }
+
+
+// dados para teste 
+// const dados = {
+//     name: "Mayk Brito",
+//     avatar: "https://avatars.githubusercontent.com/u/2254731?v=4",
+//     city: "Parana"
+// }
+
+// const naruto = {
+//     name: "Sasuke Uchiha",
+//     avatar: "https://avatars.githubusercontent.com/u/2254731?v=5",
+//     city: "Vila da folha"
+// }
+
+// const gaku = {
+//     name: "Som Goku",
+//     avatar: "https://avatars.githubusercontent.com/u/2254731?v=5",
+//     city: "Terra"
+// }
+
+// const itachi = {
+//     name: "Itachi Uchiha",
+//     avatar: "https://avatars.githubusercontent.com/u/2254731?v=5",
+//     city: "Akatsuki"
+// }
+
+// Busca os dados do servidor
+// getDados()
+
+// Envia novos dados para o servidor
+// enviarDados(gaku)
+
+// Atualiza os dados do servidor com id = 1
+// atualizarDados(5, itachi)
+
+// Deleta os dados do servidor com id = 2
+// deletarDados(5)
