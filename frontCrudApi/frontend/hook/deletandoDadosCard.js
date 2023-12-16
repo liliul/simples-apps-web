d0626e5b4d4078b827570cc09a52466079f37542
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-12-16 02:10:52 -0300

    create: hook deletandoDadosCard frontCrudApi

---
+++ frontCrudApi/frontend/hook/deletandoDadosCard.js
@@ -0,0 +1,12 @@
+const container = document.getElementById('container')
+
+container.addEventListener('click', (e) => {
+    let buttonDelete = e.target.dataset.remove;
+    if (buttonDelete) {
+        console.log(buttonDelete)
+
+        const buttonDataset = document.querySelector(`[data-id="${buttonDelete}"]`)
+        buttonDataset.remove()
+        deletarDados(buttonDelete)
+    }
+})
