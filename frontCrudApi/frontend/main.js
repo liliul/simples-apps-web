b7b42ca3b0d7b057691bb3ca105316f614b58a8a
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-12-28 02:17:26 -0300

    add: feature renderCards no main.js frontCrudApi

--- frontCrudApi/frontend/main.js
+++ frontCrudApi/frontend/main.js
@@ -1,11 +1,13 @@
 import EnviarDadosParaApi from './hook/enviandoDados.js';
-import renderCardDados from './service/reqGET.js';
+// import renderCardDados from './service/reqGET.js';
+import renderCards from './hook/renderizaCards.js';
 
 import './hook/deletandoDadosCard.js';
 import './component/buttonAtualizar.js';
 
 EnviarDadosParaApi()
-renderCardDados();
+// renderCardDados();
+renderCards()
 
 import {editarDados} from './component/buttonEditar.js'
 
