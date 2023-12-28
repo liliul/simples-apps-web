641ae9b6bc5853dae69939489ad944fc60c8d613
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-12-28 02:20:50 -0300

    chore: removendo codigo não usado frontCrudApi

--- frontCrudApi/frontend/main.js
+++ frontCrudApi/frontend/main.js
@@ -1,12 +1,10 @@
 import EnviarDadosParaApi from './hook/enviandoDados.js';
-// import renderCardDados from './service/reqGET.js';
 import renderCards from './hook/renderizaCards.js';
 
 import './hook/deletandoDadosCard.js';
 import './component/buttonAtualizar.js';
 
 EnviarDadosParaApi()
-// renderCardDados();
 renderCards()
 
 import {editarDados} from './component/buttonEditar.js'
--- frontCrudApi/frontend/service/reqGET.js
+++ frontCrudApi/frontend/service/reqGET.js
@@ -1,17 +1,4 @@
-// import renderizandoCardDados from '../component/creandoCardDados.js';
-
 const urlApiCrud = 'http://localhost:5500/api';
-
-// Crie uma função para fazer uma requisição GET para buscar os dados do servidor:
-// export default function getDados() {
-//     fetch(urlApiCrud)
-//         .then(response => response.json())
-//         .then(data => {
-//             console.log(data)
-
-//         })
-//         .catch(error => console.error(error))
-// }
 
 export default async function getDados() {
     const req = await fetch(urlApiCrud);
