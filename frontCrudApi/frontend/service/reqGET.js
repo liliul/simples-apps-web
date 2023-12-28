61a317eac145b5e9629169f9d07145b0373286f1
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-12-28 02:06:27 -0300

    update: reescrevendo a feature getDados frontCrudApi

--- frontCrudApi/frontend/service/reqGET.js
+++ frontCrudApi/frontend/service/reqGET.js
@@ -1,17 +1,21 @@
-import renderizandoCardDados from '../component/creandoCardDados.js';
+// import renderizandoCardDados from '../component/creandoCardDados.js';
 
 const urlApiCrud = 'http://localhost:5500/api';
 
 // Crie uma função para fazer uma requisição GET para buscar os dados do servidor:
-export default function getDados() {
-    fetch(urlApiCrud)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
+// export default function getDados() {
+//     fetch(urlApiCrud)
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log(data)
 
-            data.users.forEach((itens) => {
-              renderizandoCardDados(itens)
-            })
-        })
-        .catch(error => console.error(error))
+//         })
+//         .catch(error => console.error(error))
+// }
+
+export default async function getDados() {
+    const req = await fetch(urlApiCrud);
+    const res = await req.json();
+
+    return res
 }
