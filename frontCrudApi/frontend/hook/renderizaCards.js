9ad6f7bea6b6d063732c5aa80b471dadd112fdf6
liliu do carmo <32183622+liliul@users.noreply.github.com>
2023-12-30 02:20:52 -0300

    chore: jsdoc renderzaCards frontCrudApi

--- frontCrudApi/frontend/hook/renderizaCards.js
+++ frontCrudApi/frontend/hook/renderizaCards.js
@@ -1,10 +1,16 @@
 import getDados from '../service/reqGET.js';
 import renderizandoCardDados from '../component/creandoCardDados.js';
 
+/**
+ * @function
+ * @returns {string} cards rederizados no html com dados do reqGET
+ * 
+ * @description renderCards renderiza cards com dados da feature getDados  
+ * */
 export default async function renderCards() {
-	const isdata = await getDados();
-	
-	isdata.users.forEach((itens) => {
-      renderizandoCardDados(itens)
-    })
+  const isdata = await getDados();
+
+  isdata.users.forEach((itens) => {
+    renderizandoCardDados(itens)
+  })
 }
