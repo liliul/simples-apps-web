5b7a0e1ae6e6b75bba113c8f473b53082d062fc4
liliu do carmo <32183622+liliul@users.noreply.github.com>
2024-01-24 00:58:41 -0300

    fix: menu hamburger inicia aberto menuHamburger

--- menuHamburger/main.js
+++ menuHamburger/main.js
@@ -1,3 +1,3 @@
 document.querySelector('.hamburger').addEventListener('click', () => {
-	document.querySelector('.listMenu').classList.toggle('isMenu')
+	document.querySelector('#listMenu').classList.toggle('isMenu')
 })
