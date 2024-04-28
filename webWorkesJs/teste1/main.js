b8b06faf76e58506691f0be50137ab1e1814dba6
liliul <liliuestudo@gmail.com>
2024-04-28 19:59:26 -0300

    feat: mostrando mensagem do postMessage no html webWorkesJs

--- webWorkesJs/teste1/main.js
+++ webWorkesJs/teste1/main.js
@@ -3,4 +3,5 @@ const myWorker = new Worker("./worker.js");
 myWorker.postMessage("Mensagem do main loop");
 myWorker.onmessage = function (e) {
     console.log(e.data);
+    document.getElementById('main').textContent = e.data;
 }
