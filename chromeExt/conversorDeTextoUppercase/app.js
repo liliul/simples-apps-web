b8fc8d603c37c1e18f1b070c5993c1c595429ee1
liliu do carmo <32183622+liliul@users.noreply.github.com>
2024-06-29 15:58:35 -0300

    create: conversorDeTextoUppercase chromeExt

---
+++ chromeExt/conversorDeTextoUppercase/app.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function (){
+
+
+document.querySelector('#btn').addEventListener('click', function(){
+   let text	= document.querySelector('#texto').value;
+
+   document.querySelector('#resultado').innerHTML = upper(text);
+})	
+
+function upper(text){
+	return text.toUpperCase();
+}
+
+
+})
