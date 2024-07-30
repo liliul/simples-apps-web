a7297adbd189aac829979700f80fc60b9d2a00d5
liliul <liliuestudo@gmail.com>
2024-07-30 19:55:38 -0300

    feat: header style css jsModules

--- testeSimples/jsModules/main.js
+++ testeSimples/jsModules/main.js
@@ -1,1 +1,30 @@
-console.log('ok')
+const divRoot = document.getElementById('root')
+
+divRoot.innerHTML = `
+    ${content()}
+    ${h1('Naruto VS Goku')}
+`
+
+function content() {
+    return (
+        `
+            <header class='header'>
+                <div class='logo'>
+                    <span class='lo-span cor1'></span>
+                    <span class='lo-span cor2'></span>
+                    <span class='lo-span cor3'></span>
+                </div>
+
+                <div class='he-title'>
+                    ${h1('Animes ou Desenhos <strong class="he-strong">animados.</strong>')}
+                </div>
+            </header>
+        `
+    )
+}
+
+function h1(texto) {
+    return (
+        `<h1 class='h1'>${texto}</h1>`
+    )
+}
