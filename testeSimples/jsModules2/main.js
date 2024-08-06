b19221bf553a4cb30eaf2d3a63b03541b4fd5eaa
liliu do carmo <32183622+liliul@users.noreply.github.com>
2024-08-06 19:41:41 -0300

    feat: article jsModules2

--- testeSimples/jsModules2/main.js
+++ testeSimples/jsModules2/main.js
@@ -1,6 +1,11 @@
 document.getElementById('root').innerHTML = `
-  ${html('text-emerald-500 text-xl')}
+  ${article('w-full h-full flex justify-center itens-center')}
 `
+function article(tailwindcss) {
+  return (`
+    <article class='${tailwindcss}'>${html('text-emerald-500 text-xl')}</article>
+  `)
+}
 
 function html(tailwindcss) {
   return (`
