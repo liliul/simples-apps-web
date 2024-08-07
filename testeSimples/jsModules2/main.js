935a4630aad9e3afaddae967381cfca6ad5e86bf
liliu do carmo <32183622+liliul@users.noreply.github.com>
2024-08-07 17:55:51 -0300

    feat: header jsModules2

--- testeSimples/jsModules2/main.js
+++ testeSimples/jsModules2/main.js
@@ -1,5 +1,7 @@
 document.getElementById('root').innerHTML = `
-  ${article('w-full h-full flex justify-center itens-center')}
+  ${header('w-full h-60 flex justify-center items-center')}
+
+  ${article('w-full h-full flex justify-center items-center')}
 `
 function article(tailwindcss) {
   return (`
@@ -10,5 +12,25 @@ function article(tailwindcss) {
 function html(tailwindcss) {
   return (`
     <h1 class='${tailwindcss}'>Tailwindcss</h1>
+  `)
+}
+
+function header(tailwindcss) {
+  return (`
+    <header class='${tailwindcss}'>${titleHeader('w-2/4 h-48 bg-gray-800 p-2 rounded-md')}</header>
+  `)
+}
+
+
+function titleHeader(tailwindcss) {
+  return (`
+    <section class='${tailwindcss}'>
+      <ul class='w-full'>
+        <li class='text-xl text-yellow-400'>Iniciando com css</li>
+        <li class='text-xl text-green-400'>Começando com sass</li>
+        <li class='text-xl text-blue-400'>Utilizando bootstrap</li>
+        <li class='text-xl text-pink-400'>Finalizando com tailwindcss</li>
+      </ul>
+    </section>
   `)
 }
