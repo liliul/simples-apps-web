498fa7a184b879864df7de028c1bbc32fc319e76
liliu do carmo <32183622+liliul@users.noreply.github.com>
2024-08-08 21:01:27 -0300

    chore: removendo a feature article do main jsModules2

--- testeSimples/jsModules2/main.js
+++ testeSimples/jsModules2/main.js
@@ -1,19 +1,8 @@
 import { header } from './components/header.js'
-
+import { article } from './components/content.js'
+ 
 document.getElementById('root').innerHTML = `
   ${header('w-full h-60 flex justify-center items-center')}
 
   ${article('w-full h-full flex justify-center items-center')}
 `
-function article(tailwindcss) {
-  return (`
-    <article class='${tailwindcss}'>${html('text-emerald-500 text-xl')}</article>
-  `)
-}
-
-function html(tailwindcss) {
-  return (`
-    <h1 class='${tailwindcss}'>Tailwindcss</h1>
-  `)
-}
-
