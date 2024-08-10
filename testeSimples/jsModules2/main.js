e988c60bbb7b9960e429a3f93db6b94868bf2af7
liliu do carmo <32183622+liliul@users.noreply.github.com>
2024-08-09 22:10:52 -0300

    add: menuTailwindcss no main jsModules2

--- testeSimples/jsModules2/main.js
+++ testeSimples/jsModules2/main.js
@@ -1,7 +1,10 @@
 import { header } from './components/header.js'
 import { content } from './components/content.js'
+import { menuTailwindcss } from './components/meuTailwindcss.js'
 
 document.getElementById('root').innerHTML = `
+  ${menuTailwindcss}
+  
   ${header('w-full h-60 flex justify-center items-center')}
 
   ${content('w-full h-full flex justify-center items-center')}
