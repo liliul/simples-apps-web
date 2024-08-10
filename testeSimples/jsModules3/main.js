a33800c8159fd370236d9b0f50de56e708d33726
liliu do carmo <32183622+liliul@users.noreply.github.com>
2024-08-10 17:45:18 -0300

    feat: grid e style css jsModules3

--- testeSimples/jsModules3/main.js
+++ testeSimples/jsModules3/main.js
@@ -1,9 +1,33 @@
 document.getElementById('root').innerHTML = `
 	${h1()}
+
+	${grid()}
 `;
 
 function h1() {
 	return (`
 		<h1 class='h1'>function javascript</h1>
+	`)
+}
+
+function grid() {
+	return (`
+		<article class='container-card'>
+			<div class='card'></div>
+			<div class='card'></div>
+			<div class='card'></div>
+			<div class='card'></div>
+			<div class='card'></div>
+			<div class='card'></div>
+			<div class='card'></div>
+			<div class='card'></div>
+			<div class='card'></div>
+			<div class='card'></div> 
+			<div class='card'></div>
+			<div class='card'></div>
+			<div class='card'></div>
+			<div class='card'></div>
+			<div class='card'></div>
+		</article>
 	`)
 }
