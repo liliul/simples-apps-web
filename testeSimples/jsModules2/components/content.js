1679cd1aab646833153c555f7bae2e81a07443e3
liliu do carmo <32183622+liliul@users.noreply.github.com>
2024-08-09 21:31:24 -0300

    chore: mudando o nome da feature article para content jsModules2

--- testeSimples/jsModules2/components/content.js
+++ testeSimples/jsModules2/components/content.js
@@ -1,6 +1,6 @@
 import { h1 } from './utils.js'
 
-export function article(tailwindcss) {
+export function content(tailwindcss) {
   return (`
     <article class='${tailwindcss}'>${h1('text-emerald-500 text-xl')}</article>
   `)
--- testeSimples/jsModules2/main.js
+++ testeSimples/jsModules2/main.js
@@ -1,8 +1,8 @@
 import { header } from './components/header.js'
-import { article } from './components/content.js'
- 
+import { content } from './components/content.js'
+
 document.getElementById('root').innerHTML = `
   ${header('w-full h-60 flex justify-center items-center')}
 
-  ${article('w-full h-full flex justify-center items-center')}
+  ${content('w-full h-full flex justify-center items-center')}
 `
