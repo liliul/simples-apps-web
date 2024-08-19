2278f11c3873a116d3fe5b88e291e12afe0d7811
liliul <liliuestudo@gmail.com>
2024-08-19 19:51:05 -0300

    create: components  jsModules3

---
+++ testeSimples/jsModules3/components.js
@@ -0,0 +1,28 @@
+import { utils } from "./utils.js";
+
+function alerta(msg) {
+	return alert(`function and function: ${msg}`)
+}
+
+function simNao(simNao) {
+	const msTrueOrFalse = utils.trueFalse;
+	return (`
+		<h2 class='mensagem'>Mensagem: <span class='${msTrueOrFalse === true ? 'verde' : 'vermelho'}'>${simNao}</span></h2>
+	`)
+}
+
+function cardDrawcss() {
+	return (`
+		<section style='${utils.cardDrawcss}'>
+			<span style='${utils.cardDrawSpan1css}'></span>
+			<span style='${utils.cardDrawSpan2css}'></span>
+			<span style='${utils.cardDrawSpan3css}'></span>
+		</section>
+	`)
+}
+
+export const component = {
+    alerta,
+    simNao,
+    cardDrawcss
+}
