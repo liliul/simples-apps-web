009ce3c227153f20bdc816793eddc855aaeb22b4
liliul <liliuestudo@gmail.com>
2024-08-19 19:52:25 -0300

    chore: removendo feature para components jsModules3

--- testeSimples/jsModules3/main.js
+++ testeSimples/jsModules3/main.js
@@ -1,5 +1,7 @@
 import { utils } from './utils.js';
 
+import { component } from './components.js';
+
 document.getElementById('root').innerHTML = `
 	${utils.h1()}
 
@@ -8,18 +10,18 @@ document.getElementById('root').innerHTML = `
 
 function grid(msg) {
 	const mensagem = utils.trueFalse;
-	alerta(msg)
+	component.alerta(msg)
 
 	return (`
 		<article class='container-card'>
 			<div class='card'></div>
-			<div class='card'>${mensagem === true ? simNao('Ativado') : simNao('Desativado')}</div>
+			<div class='card'>${mensagem === true ? component.simNao('Ativado') : component.simNao('Desativado')}</div>
 			<div class='card'></div>
 			<div class='card'></div>
 			<div class='card'></div>
 			<div class='card'></div>
 			<div class='card'></div>
-			<div class='card'>${cardDrawcss()}</div>
+			<div class='card'>${component.cardDrawcss()}</div>
 			<div class='card'></div>
 			<div class='card'></div> 
 			<div class='card'></div>
@@ -30,26 +32,5 @@ function grid(msg) {
 				${utils.cardBtn()}
 			</div>
 		</article>
-	`)
-}
-
-function alerta(msg) {
-	return alert(`function and function: ${msg}`)
-}
-
-function simNao(simNao) {
-	const msTrueOrFalse = utils.trueFalse;
-	return (`
-		<h2 class='mensagem'>Mensagem: <span class='${msTrueOrFalse === true ? 'verde' : 'vermelho'}'>${simNao}</span></h2>
-	`)
-}
-
-function cardDrawcss() {
-	return (`
-		<section style='${utils.cardDrawcss}'>
-			<span style='${utils.cardDrawSpan1css}'></span>
-			<span style='${utils.cardDrawSpan2css}'></span>
-			<span style='${utils.cardDrawSpan3css}'></span>
-		</section>
 	`)
 }
