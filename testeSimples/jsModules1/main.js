209f49f3ae371adf089bb9864c6d7fcb496f850e
liliul <liliuestudo@gmail.com>
2024-08-22 16:48:39 -0300

    feat: main jsModules1

--- testeSimples/jsModules1/main.js
+++ testeSimples/jsModules1/main.js
@@ -1,1 +1,21 @@
-console.log('Hello World');
+document.getElementById('root').innerHTML = `
+    ${main()}
+`;
+
+function main() {
+    return (`
+        <section class='isolate'>
+            <header>
+                <h1>Header</h1>
+            </header>
+
+            <main>
+                <h1>Content</h1>
+            </main>
+
+            <footer>
+                <h1>Footer</h1>
+            </footer>
+        </section>
+    `)
+}
