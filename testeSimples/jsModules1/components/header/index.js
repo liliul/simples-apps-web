1df1732a2d94140988262792731441175c38b7b0
liliul <liliuestudo@gmail.com>
2024-08-24 15:58:47 -0300

    update: feature header jsModules1

--- testeSimples/jsModules1/components/header/index.js
+++ testeSimples/jsModules1/components/header/index.js
@@ -1,17 +1,23 @@
 function HeaderComponent() {
 
         return (`
-            <section class="w-[1000px] m-auto flex items-center justify-between mt-6">
-                <div class="w-12 h-12 bg-gray-500 rounded-lg"></div>
+            <section class="w-[1000px] h-full m-auto">
+                <section class="flex items-center justify-between mt-6">
+                    <div class="w-12 h-12 bg-gray-500 rounded-lg"></div>
 
-                <nav class="flex gap-5">
-                    <div class="w-28 h-5 bg-gray-500 rounded-lg"></div>
-                    <div class="w-40 h-5 bg-gray-500 rounded-lg"></div>
-                    <div class="w-52 h-5 bg-gray-500 rounded-lg"></div>
-                    <div class="w-20 h-5 bg-gray-500 rounded-lg"></div>
-                </nav>
+                    <nav class="flex gap-5">
+                        <div class="w-28 h-5 bg-gray-500 rounded-lg"></div>
+                        <div class="w-40 h-5 bg-gray-500 rounded-lg"></div>
+                        <div class="w-52 h-5 bg-gray-500 rounded-lg"></div>
+                        <div class="w-20 h-5 bg-gray-500 rounded-lg"></div>
+                    </nav>
 
-                <div class="w-12 h-12 bg-gray-500 rounded-full"></div>
+                    <div class="w-12 h-12 bg-gray-500 rounded-full"></div>
+                </section>
+
+                <section class="w-full h-full flex justify-center items-center">
+                    <div class="w-3/5 h-16 bg-gray-500 rounded-lg"></div>
+                </section>
             </section>
         `)
 }
