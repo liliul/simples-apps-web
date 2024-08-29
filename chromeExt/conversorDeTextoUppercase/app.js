796c71453c1f26c79ed6f61f8e63d399b06ea809
liliul <liliuestudo@gmail.com>
2024-08-29 17:29:30 -0300

    chore: removendo html do conversor para o app conversorDeTextoUppercase

--- chromeExt/conversorDeTextoUppercase/app.js
+++ chromeExt/conversorDeTextoUppercase/app.js
@@ -1,5 +1,10 @@
 document.addEventListener('DOMContentLoaded', function (){
 
+document.getElementById('root').innerHTML = `
+   <input type="text" id="texto" name="">
+	<button id="btn" >transformar</button>
+	<div id="resultado"></div>
+`;
 
 document.querySelector('#btn').addEventListener('click', function(){
    let text	= document.querySelector('#texto').value;
