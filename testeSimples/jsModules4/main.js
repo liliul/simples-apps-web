41bdd470edbf1fe94681636b8b33df5fa5300e31
liliul <liliuestudo@gmail.com>
2024-09-06 21:06:05 -0300

    feat: menu jsModules4

--- testeSimples/jsModules4/main.js
+++ testeSimples/jsModules4/main.js
@@ -1,7 +1,30 @@
 document.getElementById("root").innerHTML = `${home()}`;
 
+
 function home() {
     return (`
-        <h1 class="text-orange-500">Home</h1>
+        <header class="">${menu()}</header>
+    `)
+}
+
+
+
+function menu() {
+    return (`
+        <div class="w-[1000px] h-[100vh] m-auto bg-slate-100">
+            <div class="">
+                <h1 class="text-orange-500">Menu</h1>
+                
+                <div class="">
+                    <ul>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Exit</li>
+                        <li>Category</li>
+                        <li>Config</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     `)
 }
