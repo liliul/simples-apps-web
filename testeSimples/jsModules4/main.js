fec30b5a14a4caaff46e30dce4191a2d715492a6
liliul <liliuestudo@gmail.com>
2024-09-07 22:27:20 -0300

    update: feat menu jsModules4

--- testeSimples/jsModules4/main.js
+++ testeSimples/jsModules4/main.js
@@ -1,30 +1,12 @@
+import { menu } from './components/menu.js';
+
 document.getElementById("root").innerHTML = `${home()}`;
 
 
 function home() {
     return (`
         <div class="w-[1000px] h-[100vh] m-auto bg-slate-100">
-            <header class="ml-10">${menu()}</header>
+            <header class="ml-10">${menu.Menu()}</header>
         </div>    
-    `)
-}
-
-
-
-function menu() {
-    return (`
-            <div class="">
-                <h1 class="text-orange-500">Menu</h1>
-                
-                <div class="">
-                    <ul>
-                        <li>Home</li>
-                        <li>About</li>
-                        <li>Exit</li>
-                        <li>Category</li>
-                        <li>Config</li>
-                    </ul>
-                </div>
-            </div>
     `)
 }
