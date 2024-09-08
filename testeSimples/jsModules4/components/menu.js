f2fe680b2a993de1008e57dbeba057e33a437809
liliul <liliuestudo@gmail.com>
2024-09-07 22:23:47 -0300

    create: pasta components e feat menu jsModules4

---
+++ testeSimples/jsModules4/components/menu.js
@@ -0,0 +1,21 @@
+function Menu() {
+    return (`
+            <div class="">
+                <h1 class="text-orange-500">Menu</h1>
+                
+                <div class="">
+                    <ul>
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Exit</li>
+                        <li>Category</li>
+                        <li>Config</li>
+                    </ul>
+                </div>
+            </div>
+    `)
+}
+
+export const menu = {
+    Menu
+}
