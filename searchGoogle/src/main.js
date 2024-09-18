973ec816e057beade4e6d048109ea0c04f20ed90
liliul <liliuestudo@gmail.com>
2024-09-18 18:01:57 -0300

    create: projeto searchGoogle

---
+++ searchGoogle/src/main.js
@@ -0,0 +1,18 @@
+const searchInput  = document.getElementById('search-input');
+const searchButton = document.getElementById('button-search');
+
+function createLink() {
+    const hrefSearch  = 'search?q=';
+    const valueSearch = searchInput.value;
+
+    const linkSearch  = document.createElement('a');
+    linkSearch.href   = `${hrefSearch}${valueSearch}`;
+    linkSearch.textContent = 'Pesquisar Google';
+   
+    searchButton.innerHTML = '';
+    searchButton.appendChild(linkSearch);
+    
+    document.title = `${valueSearch} - searchGoogle`;
+}
+
+searchInput.addEventListener('change', createLink);
