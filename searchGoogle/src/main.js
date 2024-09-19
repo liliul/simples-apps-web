ba9a5aa62c589668e52137837fb68102ca6fd497
liliul <liliuestudo@gmail.com>
2024-09-19 19:25:08 -0300

    add: event currentTarget reset searchGoogle

--- searchGoogle/src/main.js
+++ searchGoogle/src/main.js
@@ -2,22 +2,24 @@ const searchInput  = document.getElementById('search-input');
 const searchButton = document.getElementById('button-search');
 const searchFrom   = document.getElementById('search-form');
 
-function createLink() {
+ function createLink() {
     const hrefSearch  = 'search?q=';
     const valueSearch = searchInput.value;
-    console.log(valueSearch)
+
     const linkSearch  = document.createElement('a');
     linkSearch.href   = `${hrefSearch}${valueSearch}`;
     linkSearch.textContent = 'Pesquisar Google';
-   
+
     searchButton.innerHTML = '';
     searchButton.appendChild(linkSearch);
-    
+
     document.title = `${valueSearch} - searchGoogle`;
 }
 
-searchFrom.addEventListener('submit', function(e) {
-    e.preventDefault()
+window.addEventListener('submit', function(event) {
+    event.preventDefault()
 
     createLink()
+
+    event.currentTarget.reset()
 });
