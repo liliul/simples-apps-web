b6a1f6796510230d0fc347b24fa195ef165bdeee
liliul <liliuestudo@gmail.com>
2024-09-22 16:12:48 -0300

    fix: express.static style css chatEmTempoReal

--- testeSimples/chatEmTempoReal/index.js
+++ testeSimples/chatEmTempoReal/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const http = require('http');
+const path = require('path');
 const socketIo = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 io.on('connection', (socket) => {
     console.log('Novo usuário conectado');
