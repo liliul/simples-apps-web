bee232e5f5ea0bb1c4bd47b387630094c942d377
liliul <liliuestudo@gmail.com>
2024-09-26 17:01:44 -0300

    update: no componente Header jsModules5

--- testeSimples/jsModules5/src/components/header/header.js
+++ testeSimples/jsModules5/src/components/header/header.js
@@ -1,7 +1,14 @@
 export function Header() {
     return (`
         <header class="w-full h-[280px]">
-            <h1 class="text-red-900">Dev SPA</h1>
+            <h1 class="text-red-900 text-3xl font-bold">Todolist</h1>
+
+            <section class="w-full h-full flex justify-center items-center mt-3">
+                <form id="form-input">
+                    <input type="text" class="w-[650px] h-[45px] outline-none border-0 rounded-lg placeholder:text-red-900 text-red-900  font-bold" placeholder="Digita sua tarefa" />
+                    <button type="submit" class="ml-2 h-[45px] bg-white pl-5 pr-5 outline-none border-0 rounded-lg text-red-900 font-bold">Criar</button>
+                </form>
+            </section>
         </header>
     `)
 }
