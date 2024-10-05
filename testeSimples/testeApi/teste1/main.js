589bc99e59cf652cd14dea59ea13c7aaf2c96e6e
liliul <liliuestudo@gmail.com>
2024-10-05 19:03:39 -0300

    chore: usando AbortController fetch api testeApi/teste1

--- testeSimples/testeApi/teste1/main.js
+++ testeSimples/testeApi/teste1/main.js
@@ -1,1 +1,21 @@
-console.log('ok')
+const urlRandomuser = 'https://randomuser.me/api/?results=15'
+
+// fetch(urlRandomuser).then(console.log)
+
+// fetch('/')
+//     .then( res => res.text())
+//     .then(console.log)
+
+const controller = new AbortController()
+
+const signal = controller.signal
+console.log(signal)
+
+fetch('/', { signal })
+.then( res => res.text())
+.then(console.log)
+.catch(console.dir)
+
+
+// controller.abort(new DOMException('(* *) mensagem'))
+// controller.abort(42)
