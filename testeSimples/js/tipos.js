e84e983ca2e1f5dd9a6535594e03b50541a043a3
liliul <liliuestudo@gmail.com>
2024-12-10 17:00:50 -0300

    feat: tipos em javascript reactTest1

---
+++ testeSimples/js/tipos.js
@@ -0,0 +1,10 @@
+class Tipos1 {
+    toString() {
+        return 'Tipos de string no javascript'
+    }
+}
+
+let tipos = new Tipos1()
+
+console.log(tipos == 'Tipos de string no javascript');
+console.log(tipos + ' naruto vs goku');
