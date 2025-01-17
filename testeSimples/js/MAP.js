06e27dc32c7d6d3e906a84479de0691724864fae
liliul <liliuestudo@gmail.com>
2025-01-17 20:42:02 -0300

    update: MAP js

--- testeSimples/js/MAP.js
+++ testeSimples/js/MAP.js
@@ -6,3 +6,16 @@ const g = mapeando.get('Goku')
 const d = mapeando.delete('Goku')
 
 console.log(s, h, g, d);
+
+console.log('----------------------------------------------------------');
+
+const ns = mapeando.set('Naruto', {nome: 'Naruto Uzumaki', type: 'Shinobi'})
+
+console.log(ns);
+
+console.log('-----------------------------------------------------------');
+
+const vs = mapeando.set('Vegeta', {nome: 'Vegeta', type: 'Sayajins'})
+const vd = mapeando.delete('Vegeta')
+console.log(vs, vd);
+
