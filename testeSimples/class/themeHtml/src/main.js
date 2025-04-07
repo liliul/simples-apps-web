2982b4948310ed8c5fc8a5788057651fea7228c6
liliul <liliuestudo@gmail.com>
2025-04-07 18:06:26 -0300

    update: themeHtml class

--- testeSimples/class/themeHtml/src/main.js
+++ testeSimples/class/themeHtml/src/main.js
@@ -20,23 +20,20 @@ class Theme {
 
         themeToggleButton.addEventListener('click', () => {
           const local = body.classList.contains('light-theme')
-            console.log(local);
 
             if (local) {
                 body.classList.add('dark-theme')
                 this.setLocalStorage('itens', 'dark-theme')
                 body.classList.remove('light-theme')
-            }
-
-            if (!local) {
+            } else {
                 body.classList.add('light-theme')
                 this.setLocalStorage('itens', 'light-theme')
                 body.classList.remove('dark-theme')
             }
             
         });
-
         
+        this.checandoTema()
     }
 
     getLocalStorage(chave) {
@@ -46,20 +43,37 @@ class Theme {
     setLocalStorage(chave, value) {
         localStorage.setItem(chave, JSON.stringify(value))
     }
+
+    checandoTema () {
+        const checkThemeStorage = theme.getLocalStorage('itens')
+    
+        if (checkThemeStorage) {
+            
+            document.querySelector('body').classList.remove('light-theme')
+            
+            document.querySelector('body').classList.add(checkThemeStorage)
+        }
+        
+    }
+    
 }
 
 const theme = new Theme()
 theme.mudarTemacomLocalStorage()
 
-function checandoTema () {
-    if (theme.getLocalStorage('itens')) {
-        console.log(theme.getLocalStorage('itens'))
+// function checandoTema () {
+//     const checkThemeStorage = theme.getLocalStorage('itens')
 
-        document.querySelector('body').classList.remove('light-theme')
+//     if (checkThemeStorage) {
+//         console.log(checkThemeStorage)
+
+//         document.querySelector('body').classList.remove('light-theme')
    
-    }
+//     }
 
-    document.querySelector('body').classList.add(theme.getLocalStorage('itens'))
-}
+//     document.querySelector('body').classList.add(theme.getLocalStorage('itens'))
+// }
 
-checandoTema()
+// checandoTema()
+
+// document.querySelector('.theme-switcher').textContent = 'Tema Dark Light'.concat(' Theme').toUpperCase() 
