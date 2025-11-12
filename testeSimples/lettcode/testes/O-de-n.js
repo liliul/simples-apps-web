b12688a067a4e8bcbab5195ec71c827374367f66
liliul <liliuestudo@gmail.com>
2025-11-12 15:38:54 -0300

    update: O-de-n testes lettcode

--- testeSimples/lettcode/testes/O-de-n.js
+++ testeSimples/lettcode/testes/O-de-n.js
@@ -26,10 +26,23 @@
 // const ver = bigO(['asdasd','bbbbb','kkkkk'])
 // console.log(ver.join(''));
 
-function bigO(arr) {
-  for (let i = arr.length - 1; i >= 0; i--) {
-    console.log(i, arr[i]);
-  }
-  return arr;
+// -----------------------------
+
+function bigO(index1, index2, index3) {
+  let arr = ['asdasd','bbbbb','kkkkk']
+
+  console.log(arr[index1], arr[index2], arr[index3])
+
+  return arr
 }
-bigO(['asdasd','bbbbb','kkkkk'])
+
+bigO(1,2,0)
+// -----------------------------
+
+// function bigO(arr) {
+//   for (let i = arr.length - 1; i >= 0; i--) {
+//     console.log(i, arr[i]);
+//   }
+//   return arr;
+// }
+// bigO(['asdasd','bbbbb','kkkkk'])
