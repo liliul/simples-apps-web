4ff21a99e8bd120ea98e448f4c0bf47dd098085b
liliul <liliuestudo@gmail.com>
2025-11-17 17:35:10 -0300

    fix: data-theme iniciando null simples-cotacao-moedas

--- simples-cotacao-moedas/src/api.js
+++ simples-cotacao-moedas/src/api.js
@@ -1,5 +1,2 @@
 import './scripts/cotacao.js';
 import './scripts/coversor.js';
-import { toggleMode } from './scripts/utils.js';
-
-toggleMode()
--- simples-cotacao-moedas/src/scripts/utils.js
+++ simples-cotacao-moedas/src/scripts/utils.js
@@ -16,34 +16,42 @@ const htmlElement = document.documentElement
 export function toggleMode() {
 
 	function darkMode() {
-		let getLocalStorage = localStorage.getItem('themeMode')
-		return htmlElement.dataset.theme = getLocalStorage
+		return htmlElement.dataset.theme = 'themeDark'
 	}
 
 	function lightMode() {
-		let getLocalStorage = localStorage.getItem('themeMode')
-		return htmlElement.dataset.theme = getLocalStorage
+		return htmlElement.dataset.theme = 'themeLight'
 	}
 
 	return { darkMode, lightMode }
 }
 
-toggleMode().darkMode()
-
 const meuToggle = document.querySelector("#meuToggle")
 const buttonCoverte = document.querySelector("#button-coverter")
-const tipotheme = 'themeDark'
+const tipotheme = localStorage.getItem('themeMode') || 'themeDark'
+const imgConverter = localStorage.getItem('imgConverter')
 
+localStorage.setItem('imgConverter', './src/assets/arrows-exchange.svg')
 meuToggle.checked = true
+
+if (tipotheme === "themeDark") {
+	htmlElement.dataset.theme = "themeDark";
+	buttonCoverte.innerHTML = '<img style="width: 15px;" src="./src/assets/arrows-dark.svg" alt="trocar moeda" />';
+	meuToggle.checked = true;
+} else {
+	htmlElement.dataset.theme = "themeLight";
+	buttonCoverte.innerHTML = `<img src="${imgConverter}" alt="trocar moeda" />`;
+	meuToggle.checked = false;
+}
 
 meuToggle.addEventListener('click', () => {
 	const mode = toggleMode()
 
-	if (htmlElement.dataset.theme === tipotheme) {
+	if (htmlElement.dataset.theme === 'themeDark') {
 		localStorage.setItem('themeMode', 'themeLight')
 		mode.lightMode()
 		meuToggle.checked = false
-		buttonCoverte.innerHTML = '<img src="./src/assets/arrows-exchange.svg" alt="trocar moeda" />'
+		buttonCoverte.innerHTML = `<img src="${imgConverter}" alt="trocar moeda" />`
 	} else {
 		localStorage.setItem('themeMode', 'themeDark')
 		mode.darkMode()
