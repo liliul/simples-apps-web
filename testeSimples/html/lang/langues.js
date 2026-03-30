d3dd3ebe17ca3b94fca9bc97c9058573696ecd53
liliul <liliuestudo@gmail.com>
2026-03-30 18:21:30 -0300

    create: traducao lang html

---
+++ testeSimples/html/lang/langues.js
@@ -0,0 +1,20 @@
+[{
+    'pt': {
+        title: 'Traduzindo com javascript.',
+        subtitle: 'O método hasOwnProperty() em JavaScript é utilizado para verificar se um objeto possui uma propriedade específica.',
+        description: 'Ele é comumente usado para evitar que propriedades herdadas de protótipos de objetos sejam verificadas. Em outras palavras, hasOwnProperty() retorna true se o objeto tiver a propriedade como sua própria (não herdada), e false caso contrário.',
+        selectLang: 'linguagem', 
+    },
+    'en': {
+        title: 'Traslater the javascript.',
+        subtitle: 'O método hasOwnProperty() em JavaScript é utilizado para verificar se um objeto possui uma propriedade específica.',
+        description: 'Ele é comumente usado para evitar que propriedades herdadas de protótipos de objetos sejam verificadas. Em outras palavras, hasOwnProperty() retorna true se o objeto tiver a propriedade como sua própria (não herdada), e false caso contrário.',
+        selectLang: 'linguagem', 
+    },
+    'esp': {
+        title: 'Traduçoes el javascript.',
+        subtitle: 'O método hasOwnProperty() em JavaScript é utilizado para verificar se um objeto possui uma propriedade específica.',
+        description: 'Ele é comumente usado para evitar que propriedades herdadas de protótipos de objetos sejam verificadas. Em outras palavras, hasOwnProperty() retorna true se o objeto tiver a propriedade como sua própria (não herdada), e false caso contrário.',
+        selectLang: 'linguagem', 
+    }
+}]
